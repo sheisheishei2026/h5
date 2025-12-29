c2eb12e2731e8a56484d3dc57ff7fd2ac9f4925e
fei <peizhifei@meituan.com>
2025-12-29 17:30:56 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pwa2/', // GitHub Pages 仓库名
+  base: '/h5/', // GitHub Pages 仓库名
 })
